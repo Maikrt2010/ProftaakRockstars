1354338a969de7067c032f30936f02b974c21ae4
Rik de Wit <rikdewit1997@gmail.com>
2020-09-24 16:07:26 +0200

    coverflow

---
+++ coverflow/script.js
@@ -0,0 +1,207 @@
+
+			coverflow('container').setup({
+				item: 5,
+				playlist: [
+					{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "lalalalal",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "lalalalal",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},
+				],
+        width: window.innerWidth,
+				coverwidth: 180,
+				coverheight: 150,
+				fixedsize: true,
+				textoffset: 50,
+				backgroundcolor: '333333',
+				backgroundopacity: 1
+      
+			});
+      
+      		coverflow('container').on('ready', function() {
+      			
+      			coverflow('container').on('click', function(index, link) {
+      
+      				alert(link);
+      			});
+      		});
+          
+          
+          
+          
+
+			  coverflow('covers1').setup({
+				item: 8,
+				playlist: [
+					{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "lalalalal",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},{
+						"title": "snow-motion",
+						"description": "what a massive and unbelievable winter we're having!",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
+						"link": "http://www.google.com/",
+						"duration": "183"
+					},
+					{
+						"title": "driving rain",
+						"description": "",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+      					"link": "http://luwes.co/",
+						"duration": "782"
+					},
+					{
+						"title": "winter brook",
+						"description": "lalalalal",
+						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
+      					"link": "http://twitter.com/",
+					},
+				],
+        width: window.innerWidth,
+				coverwidth: 180,
+				coverheight: 150,
+				fixedsize: true,
+				textoffset: 50,
+				backgroundcolor: '333333',
+				backgroundopacity: 1
+      
+			});
+      
+      		coverflow('container').on('ready', function() {
+      			
+      			coverflow('container').on('click', function(index, link) {
+      
+      				alert(link);
+      			});
+      		});
+          
+          
+          
+          
+          
