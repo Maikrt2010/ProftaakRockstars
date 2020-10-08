8c2f572d92850fad04c9854a12c751585f6025a9
Daniel <d.brodowski@student.fontys.nl>
2020-10-08 15:11:46 +0200

    Presentation layer for Sign in and Sign up created
    a viewmodel for sign up & sign in as well as the views and a controller, data from the forms is being posted and fetched by the viewmdoel.

---
+++ Front_ASP.NET/Models/SignUpViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Front_ASP.NET.Models
+{
+    public class SignUpViewModel
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+
+    }
+}
