0dfc2316ee7464628b3eaf73a930f0e368071eba
SandersonM95 <63857652+SandersonM95@users.noreply.github.com>
2020-10-28 21:17:23 +0100

    Doorklikken naar het artikel

--- coverflow/script.js
+++ coverflow/script.js
@@ -3,82 +3,134 @@
 				item: 5,
 				playlist: [
 					{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "Serverless webhosting met AWS Amplify",
+						"description": "Jeroen de Deken",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-10/802855-637330036210103252-16x9_1.jpg",
+						"link": "",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
+						"title": "Cloud center of excellence (CCOE)",
+						"description": "Marco Vervoort",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/530x530/public/2020-08/header_1.jpg",
       					"link": "http://luwes.co/",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "Lazy loading with Angular",
+						"description": "Robin van Tienhoven",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/530x530/public/2020-06/Robin%20Tienhoven_0.jpg",
+      					"link": "ArticleLazyLoading.html",
+					},
+					{
+						"title": "Van Devops naar Devsecops",
+						"description": "Christiaan Nieuwlaat",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-06/header.jpg",
+						"link": "",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
+						"title": "Devops starter",
+						"description": "Marco Vervoort",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-06/devops%20code%20header_1.jpg",
+      					"link": "",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "lalalalal",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "Security by design bij informatiesystemen",
+						"description": "Christiaan Nieuwlaat",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-04/Tech%20Burst%20omslagfoto%20.jpg",
+      					"link": "",
+					},
+					{
+						"title": "HTTP-store with Angular & RXJS",
+						"description": "Robin van Tienhoven",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-03/centralize-state-squared.jpg",
+						"link": "",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
+						"title": "Monitor residential climate with Raspberry Pi",
+						"description": "Pim Hazebroek",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-03/raspberry-pi-squared.jpg",
+      					"link": "",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "Themes in nativescript with webpack",
+						"description": "Robin van Tienhoven",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2020-03/NativeScript-6.0-1024x684_0.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Versimpel je gebruik van VM-applicaties",
+						"description": "Bart Kooijmans",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-12/sea-wood-sunset-boat-lake-river-509292-pxhere.com__0.jpg",
+						"link": "",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
+						"title": "Wijnproeven met ML.net",
+						"description": "Vincent Bitter",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-08/computer-work-wine-glass-cup-drink-472371-pxhere.com__0.jpg",
+      					"link": "",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "lalalalal",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
+						"title": "Een library migreren naar JPMS",
+						"description": "Sven Haster",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-08/java_1.jpg",
+      					"link": "",
 					},
+					{
+						"title": "Een eenvoudig genetisch algoritme",
+						"description": "Vincent Hendriks",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-06/genetisch-algoritme_0.jpg",
+      					"link": "",
+					},
+					{
+						"title": "De groene ontwikkelaar",
+						"description": "Robin Gordijn",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-06/binary-code-binary-binary-system-byte-bits-administrator-1458885-pxhere.com__1.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Structured logging in een .net applicatie",
+						"description": "Vincent Hendriks",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-05/architecture-structure-sky-window-glass-view-877703-pxhere.com_.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Maven version rules delen tussen projecten",
+						"description": "Sven Haster",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-05/laptop-desk-macbook-writing-working-technology-860609-pxhere.com_.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Van niets tot full-web-app in 10 kliks!",
+						"description": "Marco Vervoort",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-04/computer-technology-number-memory-electronic-tech-1292524-pxhere.com_.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Dit is nieuw in .net core 3.0",
+						"description": "Vincent Bitter",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-04/photo-1517148815978-75f6acaaf32c.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Software is niet voor eeuwig",
+						"description": "Lars Stolwijk",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-04/0.jpg",
+      					"link": "",
+					},
+					{
+						"title": "Lesgeven, eenvoud, kwalitatieve code",
+						"description": "Venkat Subramaniam",
+						"image": "https://www.teamrockstars.nl/sites/default/files/styles/380x380/public/2019-04/venkat.jpg",
+      					"link": "",
+					}
+
 				],
         		width: window.innerWidth,
 				coverwidth: 180,
@@ -90,7 +142,7 @@
 				textoffset: 50,
 				backgroundcolor: '333333',
 				backgroundopacity: 1,
-				mousewheel: false,
+				mousewheel: true,
       
 			});
       
@@ -101,7 +153,7 @@
 			
       			coverflow('container').on('click', function(index, link) {
       
-      				alert(link);
+      				open(link,'_self');
       			});
       		});
           
@@ -110,85 +162,75 @@
           
 
 			  coverflow('covers1').setup({
-				item: 8,
+				item: 5,
 				playlist: [
 					{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "#10 Deze ervaren .NET ontwikkelaar, en sinds kort ook Product Owner, over wat zijn definitie van groei is en hoe je duidelijke taal spreekt.",
+						"description": "Met Vincent Bitter",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+						"link": "https://open.spotify.com/episode/1fXS8ikiHTuFXjZCI87Kdi",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
+						"title": "#9 Deze DevOps evangelist steelt op dit moment de show als Cloud Solution Architect en vertelt ons hoe zijn unieke route hem hier heeft gebracht en hij geeft tips hoe je je marktwaarde kunt verhogen.",
+						"description": "Met David de Hoop",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+      					"link": "https://open.spotify.com/episode/2V9h63z2DQH2c2KNbq6g0G",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "#8 Deze platform engineer met liefde voor de pure techniek deelt zijn gouden tips voor het bijblijven in de IT, het durven stellen van vragen en vertelt over het stoeien met de beesten van...",
+						"description": "Met Subhi Dweik",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+      					"link": "https://open.spotify.com/episode/4gjOn6E51OJlhdJr1RAw7U",
+					},
+					{
+						"title": "#7 Deze software engineer leerde hoe hij van een teruggetrokken leven meer naar buiten kon treden om zo meer van het leven te genieten",
+						"description": "Met Jeroen de Deken",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+						"link": "https://open.spotify.com/episode/0iNPYFgFetz0FK8qoJrRyW",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
+						"title": "#6 Deze DevOps Cloud man vertelt in dit gesprek met 'een open einde' hoe je er soms met gestrekt been in moet gaan om voor jezelf op te komen, hoe hij de eigenaar van AnyCoin gelukkig ...",
+						"description": "Met Maarten van Arem",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+      					"link": "https://open.spotify.com/episode/7BHhv2mYnAKvvSHZQlA5nY",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "lalalalal",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "#5 Deze ervaren IT-allrounder vertelt over de kunst en de valkuil van de klant tevreden willen stellen, de misvatting over software developers en zijn recept voor groei & ontwikkeling",
+						"description": "Met Christiaan Nieuwlaat",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+      					"link": "https://open.spotify.com/episode/5G9eQiTgjwkT4EP7fPVpEJ",
+					},
+					{
+						"title": "#4 Met 20 jaar op de teller vertelt deze Limburger over hoe zijn rol als Azure Solution Architect is als een droom die uitkwam en alle stappen die hem daarbij hebben geholpen",
+						"description": "Met Marco Vervoort",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+						"link": "https://open.spotify.com/episode/5AlHMrGCtXSQJgkpuszsoV",
 						"duration": "183"
 					},
 					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
+						"title": "#3 Dit jonge talent vertelt over zijn avonturen als ondernemer, zijn huidige rol als Tech Lead, het belang van Personal Branding en hoe het hem lukt om soms zijn hoofd leeg te maken",
+						"description": "Met Sjors Snoeren",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+      					"link": "https://open.spotify.com/episode/1d98Yhn3FF8UAnL6B1SNrz",
 						"duration": "782"
 					},
 					{
-						"title": "winter brook",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},{
-						"title": "snow-motion",
-						"description": "what a massive and unbelievable winter we're having!",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/eawtAP1q-720.jpg",
-						"link": "http://www.google.com/",
+						"title": "#2 De enthousiaste 30’er die vertelt over zijn rol als Team Lead, 5 jaren plan, liefde voor de combinatie van programmeren & het coachen van mensen",
+						"description": "Met Pascal Quist",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+      					"link": "https://open.spotify.com/episode/6Sr1cfJ6S7OJHgqFNESdkH",
+					},
+					{
+						"title": "#1 Over zijn route tot Solution Architect, het belang van self-management en het durven vragen van feedback aan anderen",
+						"description": "Met Oscar van der Leij",
+						"image": "https://cdn.podcastfeed.nl/aeba5040-b60f-11ea-8880-9774aad122c1/aeba5c00-b60f-11ea-b1db-55f05061b5d6.jpg",
+						"link": "https://open.spotify.com/episode/5Q5njuuhXNMlj528moiQIZ",
 						"duration": "183"
-					},
-					{
-						"title": "driving rain",
-						"description": "",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/njZMLMwy-720.jpg",
-      					"link": "http://luwes.co/",
-						"duration": "782"
-					},
-					{
-						"title": "winter brook",
-						"description": "lalalalal",
-						"image": "http://d3el35u4qe4frz.cloudfront.net/Vg7cPUdl-720.jpg",
-      					"link": "http://twitter.com/",
-					},
+					}
 				],
 				width: window.innerWidth,
 				height:270,
@@ -198,7 +240,7 @@
 				textoffset: 50,
 				backgroundcolor: '333333',
 				backgroundopacity: 1,
-				mousewheel: false,
+				mousewheel: true,
       
 			});
       
@@ -210,7 +252,7 @@
 
       			coverflow('covers1').on('click', function(index, link) {
 
-      				alert(link);
+      				open(link);
       			});
 			  });
 
