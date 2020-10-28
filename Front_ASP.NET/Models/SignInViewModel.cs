6aac0a784d137b23b7c619733d7de3afd1e122cc
Daniel <d.brodowski@student.fontys.nl>
2020-10-28 18:16:25 +0100

    Fixed the markup of both sign in / up views

--- Front_ASP.NET/Models/SignInViewModel.cs
+++ Front_ASP.NET/Models/SignInViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace Front_ASP.NET.Models
     public class SignInViewModel
     {
 
-        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
 
     }
 }
--- Front_ASP.NET/Models/SignUpViewModel.cs
+++ Front_ASP.NET/Models/SignUpViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,19 @@ namespace Front_ASP.NET.Models
     public class SignUpViewModel
     {
 
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
     }
