1b927e17982d299d81261070f4555a6af99e2593
Maikrt2010 <Maikrt2010@gmail.com>
2020-11-05 10:40:08 +0100

    DBConnectionmssql
    Class with connectionstring aangemaakt.

---
+++ DAL/DALConnection/DBConnectionmssql.cs
@@ -0,0 +1,7 @@
+﻿namespace DAL.DALConnection
+{
+    public class DBConnectionmssql
+    {
+        private readonly string connenctionstring = "Server=mssql.fhict.local;Database=dbi364823_rockstar;User Id=dbi364823;Password=NooCyt2005";
+    }
+}
