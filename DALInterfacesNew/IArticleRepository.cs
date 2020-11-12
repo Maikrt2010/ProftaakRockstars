4581a7c2e6d07b8ce3de04014e7e607145af56ca
Maikrt2010 <Maikrt2010@gmail.com>
2020-11-12 12:58:23 +0100

    Migratie Van Bleeding Done

---
+++ DALInterfacesNew/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DALInterfaces
+{
+    public interface IArticleRepository
+    {
+        public void GetArticle();
+        public void AddArticle();
+        public void RemoveArticle();
+        public void UpdateArticle();
+    }
+}
---
+++ DALInterfacesNew/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DALInterfaces
+{
+    interface IUserRepository
+    {
+        public void GetUser();
+        public void AddUser();
+        public void RemoveUser();
+        public void UpdateUser();
+    }
+}
