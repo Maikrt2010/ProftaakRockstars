5a8c4717daa0e87ad018f0cb7f07aac8adced95c
Kevinmout <35804997+Kevinmout@users.noreply.github.com>
2020-11-17 17:06:15 +0100

    Er was iets fout gegaan. Dit is de juiste versie!
    Foto's kan je nu toevoegen. Tekst die je toegevoegd hebt, wordt weergegeven op een lege pagina.

---
+++ APIconnection/index.js
@@ -0,0 +1,21 @@
+const  express = require('express');
+const app = express();
+app.listen(3000, () => console.log("Listening at 3000"));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+app.get('/css/style.css', function(request,response){
+    response.sendFile(__dirname + '/css/style.css');
+});
+
+app.get('/css/article.css', function(request,response){
+    response.sendFile(__dirname + '/css/article.css');
+});
+
+let article;
+app.post('/api',(request, response) => {
+    article = request.body;
+    console.log('I got a request!');
+    console.log(article);
+});
+
