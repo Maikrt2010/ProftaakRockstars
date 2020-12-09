735837aef62a6af584bb6615b5f3e9a067610e06
Maikrt2010 <Maikrt2010@gmail.com>
2020-12-09 15:05:08 +0100

    User Class

---
+++ DALNew/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProftaakRockstars.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProftaakRockstars.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        DbSet<User> user { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }  
+    }
+}
---
+++ DALNew/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ProftaakRockstars.DataAccess
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string Firstname { get; set; }
+        public string Preposition { get; set; }
+        public string Lastname { get; set; }
+        public string Street { get; set; }
+        public int Housenumber { get; set; }
+        public string Addition { get; set; }
+        public string Postalcode { get; set; }
+        public string City { get; set; }
+        public DateTime Birthday { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
