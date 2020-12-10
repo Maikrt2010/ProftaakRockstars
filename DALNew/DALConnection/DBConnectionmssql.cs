be2e5ace78bf42c2f06b268c632a0ae3359b48a6
leontienhelderman <l.helderman@hotmail.com>
2020-12-10 14:16:27 +0100

    Trying to fix stuff with git cause that do be kinda scuffed.

--- DALNew/DALConnection/DBConnectionmssql.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DAL
-{
-    public class DBConnectionMssql
-    {
-        private readonly string connectionString = "Server=mssql.fhict.local;Database=dbi364823_rockstar;User Id=dbi364823_rockstar;Password=NooCyt2005";
-    }
-}
